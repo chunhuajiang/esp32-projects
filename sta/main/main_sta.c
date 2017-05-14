6f23d431006eaa6f0af5602863f0188bbc110eec
Tidy Jiang <tidyjiang@163.com>
2017-05-14 19:40:39 +0800

    update sta

--- sta/main/main_sta.c
+++ sta/main/main_sta.c
@@ -36,13 +36,10 @@ esp_err_t event_handler(void *ctx, system_event_t *event)
 
 void app_main(void)
 {
-    //nvs_flash_init();
-    
     tcpip_adapter_init();
     ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
-    //ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_FLASH) );
     ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
     wifi_config_t sta_config = {
         .sta = {
